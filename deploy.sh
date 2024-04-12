ba2e80d0ed27eb896a0bd1166ca048e33939e5c5
Urvesh109 <urveshi.t@gmail.com>
2024-04-12 15:31:47 +0530

    Add deploy script

---
+++ deploy.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+solana airdrop --keypair keypairs/creator.json 100
+
+anchor build --arch sbf
+
+anchor deploy -p bubblegum-cpi --program-keypair keypairs/deploy-key-bubblegum-cpi.json
